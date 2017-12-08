0072d08c812729a8bc3f0c9101bae167f18c6c93
Aníbal Henrique de Souza <annibalhsouza@gmail.com>
2017-12-08 18:57:51 -0200

    MYSQL_URI in enviroment

--- app/datasource.js
+++ app/datasource.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
+const MYSQL_URI = process.env.MYSQL_URI || process.env.JAWSDB_URL || 'mysql://root:3n$01Enzo@localhost:3306/node-palpitao';
 
-const mysql = new Sequelize('node-palpitao', 'root', '3n$01Enzo', {
-  host: 'localhost',
-  dialect: 'mysql',
+const mysql = new Sequelize(MYSQL_URI, {
   pool: {
     max: 5,
     min: 0,
