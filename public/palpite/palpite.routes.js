5e73a46f0225881c4c91e4ecaefd90be768e5543
Aníbal Henrique de Souza <annibalhsouza@gmail.com>
2017-12-09 21:05:35 -0200

    issue 1#: Aded routes '/' and 404

--- public/palpite/palpite.routes.js
+++ public/palpite/palpite.routes.js
@@ -1,17 +1,26 @@
 angular.module('palpite.megasena', [])
   .config(function($routeProvider, $httpProvider){ 
-    $routeProvider.when('/palpites', {
+    $routeProvider.when('/', {
       templateUrl: '/palpite/palpite.html',
       controller: 'PalpiteController',
+    })
+
+
+    $routeProvider.otherwise({
+      redirectTo: '/404',
     });
 
-    // $routeProvider.when('/palpites/megasena', {
-    //   templateUrl: '/palpite/palpite-megasena.html',
-    //   controller: 'PalpiteCtrl'
-    // });
-    $routeProvider.when('/', {
-      templateUrl: '/index.html',
-      // controller: 'PalpiteController',
+    $routeProvider.when('/404', {
+      templateUrl: '../shared/404.html',
     });
 
+    $routeProvider.when('/palpites/megasena', {
+      templateUrl: '/palpite/palpite-megasena.html',
+      controller: 'PalpiteCtrl',
+      // TODO: hot search: How immplement Google Analytics AB Test "angular"
+      // REF 01: https://github.com/peterkhayes/inlineAB
+      // title: testAB_megasena_title || [{'BR': 'Palpites para Megasena de Fim de Ano'},
+      //                                  {'BR': 'Auamente sua sorte na Megasena de Fim de Ano'}]
+    });
+    
   });
